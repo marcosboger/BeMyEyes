7818339ead33b3c51330911fa6d7a7cb8794f34a
LucaRoja <49695464+LucaRoja@users.noreply.github.com>
2019-11-19 14:04:04 -0300

    Basic Jumping
    Just the jumping mechanics, no platforms yet

---
+++ BeMyEyes/Assets/JumpingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class JumpingController : MonoBehaviour
+{
+    private Touch touch;
+    private Rigidbody2D rb;
+    private float jumpForce = 600f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Control by touching
+        if(Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("touch");
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+        }
+    }
+}

bdb30f6e109bfc013979e539732064f2045e0e95
Marcos Boger <marcos.boger@usp.br>
2019-11-30 00:44:32 +0100

    Jumping Rotation
    Jumping Rotation implemented

--- BeMyEyes/Assets/BeMyEyes/Scripts/JumpingController.cs
+++ BeMyEyes/Assets/BeMyEyes/Scripts/JumpingController.cs
@@ -7,8 +7,13 @@ public class JumpingController : MonoBehaviour
 {
     private Touch touch;
     private Rigidbody2D rb;
-    private float jumpForce = 1000f;
-
+    [SerializeField]
+    private float jumpForce = 400f;
+    [SerializeField]
+    private float _rotSpeed = 5f;
+    private bool _rotation = false;
+    [SerializeField]
+    private bool _grounded = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,31 @@ public class JumpingController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _grounded = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<BoxCollider2D>().size.y / 2 + 0.1f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+        if (_grounded)
+        {
+            rb.rotation = 0;
+            _rotation = false;
+        }
+        else
+        {
+            _rotation = true;
+        }
+
+        if (_rotation)
+        {
+            transform.Rotate(0, 0, -1 * _rotSpeed * Time.deltaTime);
+        }
         //Control by touching
-        if((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && rb.velocity.y == 0)
+        if ((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && _grounded)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
         }
+
+
+
+        
 
     }
 
